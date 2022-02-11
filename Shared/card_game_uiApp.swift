ca380d04f484aec993195e6bd241cfc180a32017
izaanjahangir <izaanjahangir2@gmail.com>
2022-02-12 02:48:35 +0500

    Initial Commit

---
+++ Shared/card_game_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_game_uiApp.swift
+//  Shared
+//
+//  Created by izaan jahangir on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct card_game_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
