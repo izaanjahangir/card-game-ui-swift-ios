f92e11151a92e178981594738b78ea37e7057f29
izaanjahangir <izaanjahangir2@gmail.com>
2022-02-12 18:14:32 +0500

    added logic to the game

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard: String = "card2";
+    @State var cpuCard: String = "card3";
+    @State var playerScore: Int = 0;
+    @State var cpuScore: Int = 0;
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -18,24 +23,44 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("deal")
+                Button(action: {
+                    let playerRandom = Int.random(in: 2...14);
+                    let cpuRandom = Int.random(in: 2...14);
+                    
+                    playerCard = "card" + String(playerRandom);
+                    cpuCard = "card" + String(cpuRandom);
+                    
+                    if(playerRandom > cpuRandom) {
+                        playerScore += 1;
+                    }
+                    
+                    else if(cpuRandom > playerRandom) {
+                        cpuScore += 1;
+                    }
+                    else {
+                        playerScore += 1;
+                        cpuScore += 1;
+                    }
+                }, label: {
+                    Image("deal")
+                })
                 Spacer()
                 HStack {
                     Spacer()
                     VStack(spacing: 20) {
                         Text("Player").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
-                        Text("0").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                        Text(String(playerScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
                     }
                     Spacer()
                     VStack(spacing: 20) {
                         Text("CPU").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
-                        Text("0").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                        Text(String(cpuScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
                     }
                     Spacer()
                 }
